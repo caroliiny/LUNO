942fec18d9b931e5e5941be4b6e277994f3f94c3
Ana Caroliny <carolmedeirosjc@gmail.com>
2024-07-06 18:31:12 -0300

    Update menu.c

--- src/menu.c
+++ src/menu.c
@@ -1,92 +1,112 @@
-#include "menu.h" // aquela funcao abrir_menu();
 #include <stdio.h>
-#include <allegro5/allegro.h> // padrao allegro
-#include <allegro5/allegro_primitives.h> // para formas primitivas, como o retangulo do botao
-#include <allegro5/allegro_image.h> // para imagens
+#include <allegro5/allegro.h> //padrao allegro
+#include <allegro5/allegro_primitives.h> //formas primitivas, como o retangulo do botao
+#include <allegro5/allegro_image.h> //para imagens
 
-typedef struct Botao { // criando o struct para os botoes e definindo o que um botao vai ter
-    int x, y, largura, altura; // aqui as variaveis basicas como a altura a largura
-    ALLEGRO_COLOR cor; // definindo a variavel para a cor
+typedef struct Botao { //cria um struct generico para botao, assim nao precisa haver redundancia de codigo
+    int x, y, largura, altura;
+    ALLEGRO_COLOR cor;
 } Botao;
 
-void desenha_Botao(Botao *botao) { //aqui para desenhaar o botao
-    al_draw_filled_rectangle(botao->x, botao->y, botao->x + botao->largura, botao->y + botao->altura, botao->cor); //desenhando um retangulo
+void desenha_Botao(Botao *botao) { //funcao para desenhar o botao na tela
+
+    al_draw_filled_rectangle(botao->x, botao->y, botao->x + botao->largura, botao->y + botao->altura, botao->cor);
 }
 
-bool clicar(Botao *botao, int mouse_x, int mouse_y) { //para saber se  o botao foi clicado
+bool clicado(Botao *botao, int mouse_x, int mouse_y) { // pra verificar se o botao foi clicado
     return mouse_x >= botao->x && mouse_x <= botao->x + botao->largura &&
            mouse_y >= botao->y && mouse_y <= botao->y + botao->altura;
 }
 
-void abrir_menu() { //implementando a funcao abrir_menu() que foi criadad naquele arquivo .h a dizendo o que ela faz >> o codigo todo do menu
-    ALLEGRO_DISPLAY* janela = al_create_display(1100, 600);
-    al_set_window_title(janela, "LUNO - Menu"); //titulo que fica la em cima da janela
 
-    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60); //temporizador
-    ALLEGRO_BITMAP* fundo = al_load_bitmap("/home/Carol/jogo/acessorios/fundo.png"); //mudar de acordo com a maquina
-    ALLEGRO_EVENT_QUEUE* event_queue = al_create_event_queue(); //evento
+int main(int argc, char **argv) {
 
-    if (!event_queue) { //verificando se os eventos foram criados, se nao, imprime essa mensagem na tela
+  
+
+
+    if (!al_init()) {
+        fprintf(stderr, "Falha ao inicializar Allegro.\n");
+        return -1;
+    }
+
+    al_init_primitives_addon(); //iniciar formas primitivas
+    al_init_image_addon(); //iniciar imagens
+    al_install_keyboard(); //para instalar o teclado
+    al_install_mouse(); //para instalar o mouse
+
+
+    ALLEGRO_DISPLAY* janela = al_create_display(1100, 600); //tamanho da janela
+    al_set_window_title(janela, "LUNO - Menu"); //titulo da janela
+
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60); // temporizador
+    ALLEGRO_BITMAP* fundo = al_load_bitmap("/home/Carol/LUNO/acessorios/fundo.png"); //mudar caminho de acordo com a maquina
+    ALLEGRO_EVENT_QUEUE* event_queue = al_create_event_queue(); //eventos
+
+
+    if (!event_queue) {
         fprintf(stderr, "Falha ao criar a fila de eventos.\n");
-
-        //caso nao criado, termina todos os eventos
         al_destroy_timer(timer);
         al_destroy_display(janela);
         al_destroy_bitmap(fundo);
-        return;
+        return -1;
     }
 
-    //registrando os eventos
-    al_register_event_source(event_queue, al_get_display_event_source(janela)); //registrando a janela
-    al_register_event_source(event_queue, al_get_timer_event_source(timer)); //registrando o temporizador
-    al_register_event_source(event_queue, al_get_keyboard_event_source()); //registrando o teclado
-    al_register_event_source(event_queue, al_get_mouse_event_source()); // registrando o mouse
+    //registrando eventos
+    al_register_event_source(event_queue, al_get_display_event_source(janela)); //janela
+    al_register_event_source(event_queue, al_get_timer_event_source(timer));//temporizador
+    al_register_event_source(event_queue, al_get_keyboard_event_source()); //teclado
+    al_register_event_source(event_queue, al_get_mouse_event_source());//mouse
 
-    //para definir a largura e altura da janela
+
+
     int largura_janela = al_get_display_width(janela);
     int altura_janela = al_get_display_height(janela);
 
-    Botao Botao_jogar = { //tudo o que tem no botao jogar
+
+    //todas as informacoes dos botoes JOGAR e SOBRE
+     Botao Botao_jogar = {
         largura_janela / 2 - 150,
         altura_janela / 2 - 28,
         260,
         50,
-        al_map_rgba(0, 0, 0, 0), //invisivel
-    };
-
-    Botao Botao_sobre = { //tudo o que tem no botao sobre
-        largura_janela / 2 - 150, 
-        altura_janela / 2 + 28,
-        260, //coordenadas y
-        50, // coordenadas x
         al_map_rgba(0, 0, 0, 0), //cor invisivel
     };
 
-    bool rodando = true; //bool pode ser true ou false, no caso rodando e true
-    bool desenha = true; // desenha e pra desenhar as coisas na tela, como o botao
+    Botao Botao_sobre = {
+        largura_janela / 2 - 150,
+        altura_janela / 2 + 28,
+        260,
+        50,
+        al_map_rgba(0, 0, 0, 0),
+    };
 
-    al_start_timer(timer);
 
-    while (rodando) { 
-        ALLEGRO_EVENT evento;
-        al_wait_for_event(event_queue, &evento);
+    //bool pode ter valores true ou false
+    bool rodando = true; 
+    bool desenha = true;
 
-        if (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+    al_start_timer(timer); //iniciar temporizador
+
+    while (rodando) { // enquanto estiver rodando
+        ALLEGRO_EVENT evento; //definindo o evento
+        al_wait_for_event(event_queue, &evento); //espera pelo evento
+
+        if (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE) { //se fechar a janela, para de rodar
             rodando = false;
+
         } else if (evento.type == ALLEGRO_EVENT_KEY_DOWN) {
             if (evento.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
                 rodando = false;
             }
+
         } else if (evento.type == ALLEGRO_EVENT_TIMER) {
             desenha = true;
+
         } else if (evento.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
-            if (clicar(&Botao_jogar, evento.mouse.x, evento.mouse.y)) { //se o clicado for o botao jogar
+            if (clicado(&Botao_jogar, evento.mouse.x, evento.mouse.y)) { //se jogar for clicado
                 printf("Botão jogar clicado!\n");
-                // abrir_nome(); por exemplo, se o nome do arquivo foi nome
-            } else if (clicar(&Botao_sobre, evento.mouse.x, evento.mouse.y)) { //se o clicado for o botao sobre
+            } else if (clicado(&Botao_sobre, evento.mouse.x, evento.mouse.y)) { //se sobre for clicado
                 printf("Botão sobre clicado!\n");
-                //aqui podemos adicionar uma funcao para mudar a tela para entrar no sobre, mas tem que ser em outro arquivo eu acho
-               
             }
         }
 
@@ -102,9 +122,11 @@ void abrir_menu() { //implementando a funcao abrir_menu() que foi criadad naquel
         }
     }
 
-    //encerrando os eventos
+    //encerra os eventos
     al_destroy_event_queue(event_queue);
     al_destroy_timer(timer);
     al_destroy_display(janela);
     al_destroy_bitmap(fundo);
+
+    return 0;
 }
