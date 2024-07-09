69f313bed5441ba13f2741e173ba190b19a0c354
Ana Caroliny <carolmedeirosjc@gmail.com>
2024-07-09 20:36:58 -0300

    Update historia.c

--- src/historia.c
+++ src/historia.c
@@ -1,15 +1,23 @@
-//FUNCIONANDO OK
 #include <stdio.h>
-#include <ctype.h>
-#include <string.h>
 #include <allegro5/allegro.h>
-#include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
-#include <stdbool.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
 
-#define tamNome 20
+typedef struct Botao {
+    int x, y, largura, altura;
+    ALLEGRO_COLOR cor;
+} Botao;
+
+void desenha_Botao(Botao *botao) {
+    al_draw_filled_rectangle(botao->x, botao->y, botao->x + botao->largura, botao->y + botao->altura, botao->cor);
+}
+
+bool clicado(Botao *botao, int mouse_x, int mouse_y) {
+    return mouse_x >= botao->x && mouse_x <= botao->x + botao->largura &&
+           mouse_y >= botao->y && mouse_y <= botao->y + botao->altura;
+}
 
 int main(int argc, char **argv) {
     if (!al_init()) {
@@ -17,28 +25,42 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    al_init_image_addon(); // Inicializa addon de imagens
-    al_init_font_addon();  // Inicializa addon de fontes
-    al_init_ttf_addon();   // Inicializa addon de TrueType Fonts
-    al_install_keyboard(); // Instala o teclado
-    al_install_mouse();    // Instala o mouse
+    al_init_primitives_addon();
+    al_init_image_addon();
+    al_install_keyboard();
+    al_install_mouse();
+    al_install_audio();
+    al_init_acodec_addon();
+    
+    if (!al_reserve_samples(1)) {
+        fprintf(stderr, "Falha ao reservar canais de áudio.\n");
+        return -1;
+    }
 
-    ALLEGRO_DISPLAY* janela = al_create_display(1100, 600);
-    al_set_window_title(janela, "LUNO - Inserir nome");
+    ALLEGRO_DISPLAY *janela = al_create_display(1100, 600);
+    if (!janela) {
+        fprintf(stderr, "Falha ao criar a janela.\n");
+        return -1;
+    }
+    al_set_window_title(janela, "LUNO - Menu");
 
-    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60);
-    ALLEGRO_BITMAP* fundo1 = al_load_bitmap("/home/Carol/LUNO/acessorios/nome.png");
-    ALLEGRO_BITMAP* fundo2 = al_load_bitmap("/home/Carol/LUNO/acessorios/nome2.png");
-    ALLEGRO_BITMAP* fundo3 = al_load_bitmap("/home/Carol/LUNO/acessorios/hist1.png"); 
-    ALLEGRO_BITMAP* fundo4 = al_load_bitmap("/home/Carol/LUNO/acessorios/hist2.png"); 
-    ALLEGRO_BITMAP* fundo5 = al_load_bitmap("/home/Carol/LUNO/acessorios/hist3.png"); 
-    ALLEGRO_BITMAP* fundo6 = al_load_bitmap("/home/Carol/LUNO/acessorios/hist4.png"); 
-    ALLEGRO_BITMAP* fundo7 = al_load_bitmap("/home/Carol/LUNO/acessorios/hist5.png"); 
-    ALLEGRO_BITMAP* fundo8 = al_load_bitmap("/home/Carol/LUNO/acessorios/hist6.png"); 
-    ALLEGRO_BITMAP* fundo = fundo1; // a imagem quando inicia o programa
-    ALLEGRO_EVENT_QUEUE* fila_eventos = al_create_event_queue();
+    ALLEGRO_TIMER *timer = al_create_timer(1.0 / 60);
+    if (!timer) {
+        fprintf(stderr, "Falha ao criar o timer.\n");
+        al_destroy_display(janela);
+        return -1;
+    }
 
-    if (!fila_eventos) {
+    ALLEGRO_BITMAP *fundo = al_load_bitmap("/home/carol/LUNO/acessorios/fundo.png");
+    if (!fundo) {
+        fprintf(stderr, "Falha ao carregar a imagem de fundo.\n");
+        al_destroy_timer(timer);
+        al_destroy_display(janela);
+        return -1;
+    }
+
+    ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
+    if (!event_queue) {
         fprintf(stderr, "Falha ao criar a fila de eventos.\n");
         al_destroy_timer(timer);
         al_destroy_display(janela);
@@ -46,115 +68,97 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    al_register_event_source(fila_eventos, al_get_display_event_source(janela));
-    al_register_event_source(fila_eventos, al_get_timer_event_source(timer));
-    al_register_event_source(fila_eventos, al_get_keyboard_event_source());
-    al_register_event_source(fila_eventos, al_get_mouse_event_source());
-
-    ALLEGRO_FONT* fonte = al_load_ttf_font("/home/Carol/LUNO/acessorios/DejaVuSans-Bold.ttf", 30, 0);
-
-    if (!fonte) {
-        fprintf(stderr, "Falha ao carregar fonte.\n");
-        al_destroy_event_queue(fila_eventos);
-        al_destroy_timer(timer);
-        al_destroy_display(janela);
-        al_destroy_bitmap(fundo);
-        return -1;
-    }
-
-    char nome[tamNome] = ""; // Inicializa a string do nome
-    int posicao_texto = 0;   // Posição atual do texto digitado
+    al_register_event_source(event_queue, al_get_display_event_source(janela));
+    al_register_event_source(event_queue, al_get_timer_event_source(timer));
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
+    al_register_event_source(event_queue, al_get_mouse_event_source());
 
     int largura_janela = al_get_display_width(janela);
     int altura_janela = al_get_display_height(janela);
 
+    Botao Botao_jogar = {
+        largura_janela / 2 - 150,
+        altura_janela / 2 - 28,
+        260,
+        50,
+        al_map_rgba(0, 0, 0, 0), //cor invisível
+    };
+
+    Botao Botao_sobre = {
+        largura_janela / 2 - 150,
+        altura_janela / 2 + 28,
+        260,
+        50,
+        al_map_rgba(0, 0, 0, 0),
+    };
+
+    // Carrega a música de fundo
+    ALLEGRO_SAMPLE *som = al_load_sample("/home/carol/LUNO/acessorios/musicaintro.mp3");
+    if (!som) {
+        fprintf(stderr, "Falha ao carregar a música.\n");
+        al_destroy_timer(timer);
+        al_destroy_display(janela);
+        al_destroy_bitmap(fundo);
+        al_destroy_event_queue(event_queue);
+        return -1;
+    }
+
+    // Inicia a reprodução da música em loop
+    if (!al_play_sample(som, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_LOOP, NULL)) {
+        fprintf(stderr, "Falha ao reproduzir a música.\n");
+        al_destroy_sample(som);
+        al_destroy_timer(timer);
+        al_destroy_display(janela);
+        al_destroy_bitmap(fundo);
+        al_destroy_event_queue(event_queue);
+        return -1;
+    }
+
     bool rodando = true;
-    bool redesenhar = true;
+    bool desenha = true;
 
     al_start_timer(timer);
 
     while (rodando) {
         ALLEGRO_EVENT evento;
-        al_wait_for_event(fila_eventos, &evento);
+        al_wait_for_event(event_queue, &evento);
 
         if (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
             rodando = false;
         } else if (evento.type == ALLEGRO_EVENT_KEY_DOWN) {
             if (evento.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
                 rodando = false;
-            } else if (evento.keyboard.keycode == ALLEGRO_KEY_ENTER) {
-                // Troca a imagem ao pressionar Enter
-                if (fundo == fundo1) {
-                    fundo = fundo2;
-                } else if (fundo == fundo2) {
-                    fundo = fundo3;
-                     // Limpar o nome digitado
-                    nome[0] = '\0';
-                    posicao_texto = 0;
-                } else if (fundo == fundo3) {
-                    fundo = fundo4;
-                } else if (fundo == fundo4) {
-                    fundo = fundo5;
-                } else if (fundo == fundo5) {
-                    fundo = fundo6;
-                } else if (fundo == fundo6) {
-                    fundo = fundo7;
-                } else if (fundo == fundo7) {
-                    fundo = fundo8;
-                } //else {
-                    //abrir_jogo();
-                //}
-
-            }
-        } else if (evento.type == ALLEGRO_EVENT_KEY_CHAR) {
-            // Verifica se foi digitado um caractere ASCII
-            int codigo_tecla = evento.keyboard.unichar;
-            if (codigo_tecla >= 32 && codigo_tecla <= 126) {
-                if (posicao_texto < tamNome - 1) {
-                    nome[posicao_texto++] = (char)codigo_tecla;
-                    nome[posicao_texto] = '\0';
-                }
-            } else if (evento.keyboard.keycode == ALLEGRO_KEY_BACKSPACE) {
-                
-                if (posicao_texto > 0) {
-                    posicao_texto--;
-                    nome[posicao_texto] = '\0';
-                }
             }
         } else if (evento.type == ALLEGRO_EVENT_TIMER) {
-            redesenhar = true;
+            desenha = true;
+        } else if (evento.type == ALLEGRO_EVENT_MOUSE_AXES) {
+            if (clicado(&Botao_jogar, evento.mouse.x, evento.mouse.y)) {
+                // Abrir tela de jogo
+            } else if (clicado(&Botao_sobre, evento.mouse.x, evento.mouse.y)) {
+                // Abrir tela "Sobre"
+            }
+            desenha = true;
         }
 
-        if (redesenhar && al_is_event_queue_empty(fila_eventos)) {
-            redesenhar = false;
+        if (desenha && al_is_event_queue_empty(event_queue)) {
+            desenha = false;
             al_clear_to_color(al_map_rgb(255, 255, 255));
-            al_draw_scaled_bitmap(fundo, 0, 0, al_get_bitmap_width(fundo), al_get_bitmap_height(fundo),
-                                  0, 0, largura_janela, altura_janela, 0);
+            al_draw_scaled_bitmap(fundo, 0, 0, al_get_bitmap_width(fundo), al_get_bitmap_height(fundo), 0, 0, largura_janela, altura_janela, 0);
 
-
-            if (fonte) {
-                al_draw_text(fonte, al_map_rgb(206, 50, 250), 700, 130, 0, nome); //desenha a fonte com a cor e as coordenadas pedidas
-            }
+            desenha_Botao(&Botao_jogar);
+            desenha_Botao(&Botao_sobre);
 
             al_flip_display();
         }
     }
 
-    //encerrando os eventos ao sair do loop
-
-    al_destroy_font(fonte);
-    al_destroy_event_queue(fila_eventos);
+    // Libera a amostra de áudio e demais recursos
+    al_destroy_sample(som);
     al_destroy_timer(timer);
     al_destroy_display(janela);
-    al_destroy_bitmap(fundo1);
-    al_destroy_bitmap(fundo2);
-    al_destroy_bitmap(fundo3);
-    al_destroy_bitmap(fundo4);
-    al_destroy_bitmap(fundo5);
-    al_destroy_bitmap(fundo6);
-    al_destroy_bitmap(fundo7);
-    al_destroy_bitmap(fundo8);
+    al_destroy_bitmap(fundo);
+    al_destroy_event_queue(event_queue);
+    al_uninstall_audio();
 
     return 0;
 }
-
